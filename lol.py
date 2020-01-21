41ce6a5d6fad57ecd18c898d6e364f7a62dd7ba7
shreyansh000 <55754035+shreyansh000@users.noreply.github.com>
2020-01-21 17:02:28 +0530

    Create lol.py

---
+++ lol.py
@@ -0,0 +1,47 @@
+from telethon import events
+
+import asyncio
+
+
+
+
+
+
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 2
+
+    animation_ttl = range(0, 11)
+
+    
+     input_str == "hack":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+        
+            "`Connecting To Hacked Private Server...`",
+            "`Target Selected.`",
+            "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
+            "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
+            "`Hacking... 100%\n█████████HACKED███████████ `",
+            "`Targeted Account Hacked...\n\nPay 999$ To Satwik To Remove This Hack`"
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 11])
+
+
